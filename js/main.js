8349ccd0eb0989f83dc254002a85d1863e212a7f
Yahia Muhammad <106348723+Yahia-Muhammad@users.noreply.github.com>
2022-06-18 21:11:46 +0200

    Project Upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Start Light Slide */
 $(document).ready(function() {
     $('#autoWidth').lightSlider({
         autoWidth:true,
@@ -7,9 +8,9 @@ $(document).ready(function() {
         } 
     });  
   });
-/*******************************************************************************/
+/* End Light Slide */
+/* Start OnScroll */
 window.onscroll = function(){
-    console.log(window.pageYOffset);
     var myUp = window.pageYOffset;
     if(myUp >= 300 && myUp <= 1077){
         document.getElementById("html").style.width = "90%"
@@ -20,9 +21,28 @@ window.onscroll = function(){
         document.getElementById("js").style.width = "0%"
         document.getElementById("photo").style.width = "0%"
     }
-    console.log(window.pageYOffset);
 }
+/* End OnScroll */
+/* Start OnLoad */
+window.onload = function(){
+    allScreenActive();
+    if(window.innerWidth >= 1024){
+        activiteAllAnimate();
+    }else{
+        hidContact();
+    }
+}
+/* End OnLoad */
+/* Start OnResize */
+window.onresize = function(){
 
+    allScreenActive();
+
+}
+/* End OnResize */
+
+
+/* Start Animate Page-3 Left */
 var myBoxs = document.querySelectorAll("#boxs");
 
 document.getElementById("start").onclick = function(){
@@ -44,42 +64,138 @@ document.getElementById("end").onclick = function(){
         box.style.justifyContent = "flex-end";
     });
 }
+/* End Animate Page-3 Left */
+/* Start Btn On Up */
+var myRightBtn = document.getElementById("right-node");
 
-document.getElementById("right-node").onclick = function(){
+myRightBtn.onclick = function(){
     window.scrollTo(0,0);
 }
-
-
+/* End Btn On Up */
+/* Start Up Icon Page-1 */
 var myIconsPageOne = document.querySelectorAll("#icons");
 var myIconPOne = document.querySelectorAll("#icons-p");
 
-
-for(var i = 0; i <= 3; i++){
-    console.log(myIconsPageOne[i]);
+function upp(num1, num2, num3){
+    myIconsPageOne[0].onclick = function(){
+        window.scrollTo(0, num1);
+    }
+    myIconsPageOne[1].onclick = function(){
+        window.scrollTo(0, num2);
+    }
+    myIconsPageOne[2].onclick = function(){
+        window.scrollTo(0, num3);
+    }
+    
+    myIconPOne[0].onclick = function(){
+        window.scrollTo(0, num1);
+    }
+    myIconPOne[1].onclick = function(){
+        window.scrollTo(0, num2);
+    }
+    myIconPOne[2].onclick = function(){
+        window.scrollTo(0, num3);
+    }
+    return;
 }
 
-myIconsPageOne[0].onclick = function(){
-    window.scrollTo(0, 660);
-}
-myIconsPageOne[1].onclick = function(){
-    window.scrollTo(0, 1000);
-}
-myIconsPageOne[2].onclick = function(){
-    window.scrollTo(0, 1290);
+function allScreenActive(myScreenOne){
+    if(window.innerWidth >= 1440){
+
+        myScreenOne = upp(675, 1030, 1268);
+
+    }else if(window.innerWidth >= 1024){
+
+        myScreenOne = upp(577, 930, 1233);
+
+    }else if(window.innerWidth >= 768){
+
+        myScreenOne = upp(580, 930, 1219);
+
+    }else if(window.innerWidth >= 425){
+
+        myScreenOne = upp(433, 740, 1202);
+
+    }else if(window.innerWidth >= 375){
+
+        myScreenOne = upp(433, 735, 1209);
+
+    }else if(window.innerWidth >= 320){
+
+        myScreenOne = upp(435, 735, 1214);
+
+    }
+    return;
+
 }
 
-var myIconsPageOneP = document.querySelectorAll("#icons-p");
+/* End Up Icon Page-1 */
+/* Start Animate Larg Screen */
 
-for(var i = 0; i <= 3; i++){
-    console.log(myIconsPageOneP[i]);
+function activiteAllAnimate(){
+    iconPageOne();
+    btnUp();
+    sochialLeft();
 }
 
-myIconsPageOneP[0].onclick = function(){
-    window.scrollTo(0, 660);
-}
-myIconsPageOneP[1].onclick = function(){
-    window.scrollTo(0, 1000);
-}
-myIconsPageOneP[2].onclick = function(){
-    window.scrollTo(0, 1290);
-}
+    function iconPageOne(){
+
+        for(var z = 0; z <= myIconsPageOne.length - 1; z++){
+
+            myIconsPageOne[z].onmouseenter = function(){
+                this.style.fontSize = "300%";
+                this.style.backgroundColor = "#ff3a2a";
+                this.style.color = "#fff";
+            }
+            myIconsPageOne[z].onmouseleave = function(){
+                this.style.fontSize = "265%";
+                this.style.backgroundColor = "#fff";
+                this.style.color = "#000";
+            }
+            myIconPOne[z].onmouseenter = function(){
+                this.style.backgroundColor = "#ff3a2a";
+                this.style.color = "#fff";
+            }
+            myIconPOne[z].onmouseleave = function(){
+                this.style.backgroundColor = "#fff";
+                this.style.color = "#000";
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    
+    function btnUp(){
+        myRightBtn.onmouseenter = function(){
+            this.style.fontSize = "65px"
+        }
+        myRightBtn.onmouseleave = function(){
+            this.style.fontSize = "55px"
+        }
+    }
+    function sochialLeft(){
+        var myBtnLeft = document.querySelectorAll("#link-left");
+
+        for(var x = 0; x <= myBtnLeft.length -1; x++){
+
+            myBtnLeft[x].onmouseenter = function(){
+                this.style.fontSize = "40px"
+            }
+
+            myBtnLeft[x].onmouseleave = function(){
+                this.style.fontSize = "30px"
+            }
+        }
+        return;
+    }
+    function hidContact(){
+        document.getElementById("hid-input").style.display = "none";
+    }
